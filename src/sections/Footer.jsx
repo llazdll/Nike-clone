f09e414d61edce75e0d25a0aa9c2fc207a348331
theho3scein <102923017+theho3scein@users.noreply.github.com>
2024-07-18 23:01:27 +0330

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -65,6 +65,7 @@ const Footer = () => {
           <p>Copyright. All rights reserved.</p>
         </div>
         <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
+        <p className='font-montserrat cursor-pointer'>iiazdii</p>
       </div>
     </footer>
   );
